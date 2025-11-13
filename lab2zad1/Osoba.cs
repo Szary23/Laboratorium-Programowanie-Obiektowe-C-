f44af9e0ec2bf6b5724761379e800f6df55d7c90
Szary23 <dmnszary@gmail.com>
2025-11-13 21:20:17 +0100

    lab2zad1

---
+++ lab2zad1/Osoba.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+///Napisz klasę Osoba, która będzie przechowywać informacje o imieniu, nazwisku oraz wieku osoby.
+///Zaimplementuj konstruktor, który będzie przyjmował wszystkie trzy wartości.
+///Użyj właściwości Imie, Nazwisko, Wiek, z walidacją:
+///Imię i Nazwisko muszą mieć co najmniej 2 znaki.
+///Wiek musi być liczbą dodatnią.
+///Zaimplementuj metodę WyswietlInformacje(), która wyświetli informacje o osobie.
+
+namespace lab2zad1
+{
+    internal class Osoba
+    {
+
+        private string imie, nazwisko;
+        private int wiek;
+        //zmienna imie
+        public string Imie
+        {
+            get
+            {
+                return imie;
+            }
+            set
+            {
+                if (value.Length >= 2)
+                {
+                    imie = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Imię musi mieć co najmniej 2 znaki.");
+                }
+            }
+        }
+        //zmienna nazwisko   
+        public string Nazwisko
+        {
+            get
+            {
+                return nazwisko;
+            }
+            set
+            {
+                if (value.Length >= 2)
+                {
+                    nazwisko = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Nazwisko musi mieć co najmniej 2 znaki.");
+                }
+            }
+        }
+        //zmienna wiek
+        public int Wiek
+        {
+            get
+            {
+                return wiek;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    wiek = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Wiek musi być liczbą dodatnią.");
+                }
+            }
+        }
+
+        public Osoba(string imie, string nazwisko, int wiek)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Wiek = wiek;
+        }
+
+        public void WyswietlInformacje()
+        {
+            Console.WriteLine($"Imię: {Imie}, Nazwisko: {Nazwisko}, Wiek: {Wiek}");
+        }
+
+    }
+}
---
+++ lab2zad1/Program.cs
@@ -0,0 +1,7 @@
+﻿using lab2zad1;
+
+Osoba osoba1 = new Osoba("Kacper", "Nowak", 23);
+Osoba osoba2 = new Osoba("Michał", "Gruszka", 40);
+
+osoba1.WyswietlInformacje();
+osoba2.WyswietlInformacje();

fa184044eb925cffda4cf6902c6048a056c2f5d9
Szary23 <dmnszary@gmail.com>
2025-11-13 21:24:59 +0100

    lab2zad2

---
+++ Lab2zad2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Przykład użycia:
+//BankAccount konto = new BankAccount("Jan Kowalski", 1000);
+//konto.Wplata(500);
+//konto.Wyplata(200);
+//Console.WriteLine($"Saldo: {konto.Saldo}");
+
+
+using lab2zad2;
+
+BankAccount konto = new BankAccount("Jan Kowalski", 1000);
+konto.Wplata(500);
+konto.Wyplata(200);
+Console.WriteLine($"Saldo: {konto.Saldo}");
